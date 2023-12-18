12ebd5f943633f7cbc77c44ed4bb69aaeb1fe31b
SeparateThroughTime <63103971+SeparateThroughTime@users.noreply.github.com>
2023-12-18 23:03:16 +0100

    Spooky shader - Darkening and flickering implemented

--- shaders/spooky.fragment.glsl
+++ shaders/spooky.fragment.glsl
@@ -11,6 +11,13 @@ uniform vec2 resolution;
 
 out vec4 color;
 
+// Random Boolean.
+// theshold determines the possibility for true.
+// invertedDuration the time it stays true/false in seconds.
+bool randomBool(float threshold, float invertedDuration) {
+    return step(fract(sin(floor(time * invertedDuration) * 100002.423458)), threshold) == 1.0 ? true : false;
+}
+
 vec4 darkening() {
     return vec4(vec3(min(1, abs(sin(time * 0.25) * cos(time * 0.1)) + 0.25)), 1.0);
 }
@@ -20,19 +27,18 @@ vec4 flickering() {
     float fractFastTime = fract(fastTime);
     float floorFastTime = floor(fastTime);
 
-    //    if(!(floorFastTime % 2 == 0 && floorFastTime % 5 == 0 && floorFastTime % 7 == 0)) {
-    //        return 0.0;
-    //    }
+    if (randomBool(0.95, 5)) {
+        return vec4(0.0);
+    }
 
     float impulse = fractFastTime * 8.0;
-    return vec4(vec3(impulse * exp(1.0 - impulse)), 1.0);
+    return vec4(vec3(impulse * exp(1.0 - impulse)), 0.0);
 }
 
 void main() {
     if (fTexId > 0) {
         int id = int(fTexId);
         color = texture(uTextures[id], fTexCoords) * darkening();
-        color = texture(uTextures[id], fTexCoords);
         color += vec4(vec3(flickering() * color.a), 0.0);
     } else {
         color = fColor;
